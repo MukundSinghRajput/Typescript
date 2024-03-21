409f296eb94dbe0ee4e36cbad8791b9b015a7969
Mukund <mukundsrajput@gmail.com>
2024-03-21 21:49:47 +0530

    Typescript

--- 10/app.js
+++ 10/app.js
@@ -1,2 +1,9 @@
 var anchor = document.querySelector("a");
 console.log(anchor === null || anchor === void 0 ? void 0 : anchor.href);
+// OR 
+var anc = document.querySelector("a");
+console.log(anc.href);
+// const form = document.querySelector("form")!
+// const particularform = document.getElementsByClassName('new-item-form')
+var particularform = document.querySelector(".new-item-form");
+console.log(particularform);
--- 10/app.ts
+++ 10/app.ts
@@ -1,2 +1,13 @@
-const anchor = document.querySelector("a")
+const anchor = document.querySelector("a");
 console.log(anchor?.href)
+
+// OR 
+
+const anc = document.querySelector("a")!;
+console.log(anc.href)
+
+// const form = document.querySelector("form")!
+
+// const particularform = document.getElementsByClassName('new-item-form')
+const particularform = document.querySelector(".new-item-form") as HTMLFormElement
+console.log(particularform)
